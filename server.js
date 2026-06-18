92718550999ab16a0effc1bca2988c1359c2d272
duvanpalomino-glitch <juandiegojuliopalomino@DiegoRemix.local>
2026-06-17 19:14:19 -0500

    actualizacion del archivo server,clase 30

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 const rutaCandidatos = path.join(__dirname, "data", "candidatos.json");
+const rutaVotos = path.join(__dirname, "data", "votos.json");
+
+function leerVotos() {
+  const data = fs.readFileSync(rutaVotos, "utf8");
+  return JSON.parse(data);
+}
+
+function guardarVotos(votos) {
+  fs.writeFileSync(rutaVotos, JSON.stringify(votos, null, 2));
+}
 
 function leerCandidatos() {
   const data = fs.readFileSync(rutaCandidatos, "utf8");
@@ -48,6 +58,13 @@ app.post("/api/candidatos", function (req, res) {
     candidato: nuevoCandidato
   });
 });
+
+app.get("/api/votos", function (req, res) {
+  const votos = leerVotos();
+  res.json(votos);
+});
+
+
 
 app.listen(PORT, function () {
   console.log("Servidor funcionando en http://localhost:" + PORT);
